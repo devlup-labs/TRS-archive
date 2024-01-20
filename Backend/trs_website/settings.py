7d919727860e79458a4b723b2de6156bae5815ba
AdityaRathor <adityarathor120@gmail.com>
2024-01-21 03:55:23 +0530

    email verification via otp 2

--- Backend/trs_website/settings.py
+++ Backend/trs_website/settings.py
@@ -190,5 +190,5 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER='adityarathor120@gmail.com'
-EMAIL_HOST_PASSWORD='jihp qpgd heoh lusy'
+EMAIL_HOST_USER='your email id'
+EMAIL_HOST_PASSWORD='your app password'
