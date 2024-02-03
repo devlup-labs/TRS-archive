f9a884eaa5c78dd3f7c36725757e9a2a64b07bbb
Govind Mali <mali.5@iitj.ac.in>
2024-02-03 23:00:42 +0530

    Ewmoved un-necessary code

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -17,31 +17,6 @@ class User(AbstractUser):
     REQUIRED_FIELDS = ['username']
 
 
-    # def profile(self):
-    #     profile = Profile.objects.get(user=self)
-
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     full_name = models.CharField(max_length=1000)
-#     bio = models.CharField(max_length=100)
-#     image = models.ImageField(upload_to="user_images", default="default.jpg")
-#     upload_verified = models.BooleanField(default=False)
-
-
-# def create_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         profile=Profile.objects.create(user=instance)
-    
-#     if ".iit" in instance.email:
-#             profile.upload_verified = True
-#             profile.save()
-
-# def save_user_profile(sender, instance, **kwargs):
-#     instance.profile.save()
-
-# post_save.connect(create_user_profile, sender=User)
-# post_save.connect(save_user_profile, sender=User)
-
 class Posts(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
     title = models.CharField(max_length=100)
