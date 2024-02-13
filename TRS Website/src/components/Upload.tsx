52710478f8ff5c54ce36861595cdcf4d36f3e5a7
sriganeshres <sriganeshthota12345@gmail.com>
2024-02-13 17:32:05 +0530

    Added a new Search and Made changes to Upload

--- TRS Website/src/components/Upload.tsx
+++ TRS Website/src/components/Upload.tsx
@@ -1,65 +1,74 @@
+import { jwtDecode } from "jwt-decode";
 import { ChangeEvent, useState, useRef, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export const Upload = () => {
+  const navigate = useNavigate();
   const fileInputRef = useRef<HTMLInputElement>(null);
+
   useEffect(() => {
     // Focus the file input when the component mounts
     if (fileInputRef.current) {
       fileInputRef.current.focus();
     }
   }, []);
-  const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
-  const [filePreview, setFilePreview] = useState<string[]>([]);
+
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
-  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const files = e.target.files;
-    if (files) {
-      const newFiles = Array.from(files);
-      setSelectedFiles([...selectedFiles, ...newFiles]);
+  const [email, setEmail] = useState("");
+  const [category, setCategory] = useState("");
+  const [subCategory, setSubCategory] = useState("");
 
-      const newPreviews = newFiles.map((file) => URL.createObjectURL(file));
-      setFilePreview([...filePreview, ...newPreviews]);
+  useEffect(() => {
+    const token = localStorage.getItem("authTokens");
+    if (token) {
+      setEmail(jwtDecode(JSON.parse(token).access).email);
+    } else {
+      navigate("/login");
+    }
+  }, [navigate]);
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setSelectedFile(file);
     }
   };
-  const handleFileRemove = (index: number) => {
-    const updatedFiles = [...selectedFiles];
-    const updatedPreviews = [...filePreview];
 
-    URL.revokeObjectURL(updatedPreviews[index]); // Revoke the object URL
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-    updatedFiles.splice(index, 1);
-    updatedPreviews.splice(index, 1);
-
-    setSelectedFiles(updatedFiles);
-    setFilePreview(updatedPreviews);
-  };
-  const handleSubmit = async () => {
+    if (!selectedFile) {
+      alert("Please select a file");
+      console.error("Please select a file");
+      return;
+    }
+    console.log(selectedFile);
     try {
-      const formData = new FormData();
-
-      selectedFiles.forEach((file) => {
-        formData.append("files", file);
-      });
-
       const response = await fetch("http://127.0.0.1:8000/api/upload/", {
         method: "POST",
         body: JSON.stringify({
+          user: email,
           title,
           body,
-          formData,
+          Document: selectedFile,
+          // category,
+          // subCategory
         }),
       });
 
       if (response.ok) {
-        console.log("Files successfully uploaded to the backend");
-        // Optionally, you can reset the selectedFiles and filePreview state after successful upload
-        setSelectedFiles([]);
-        setFilePreview([]);
+        console.log("File successfully uploaded to the backend");
+        setTitle("");
+        setBody("");
+        setSelectedFile(null);
+        navigate("/");
       } else {
-        console.error("Failed to upload files to the backend");
+        console.error("Failed to upload file to the backend");
       }
     } catch (error) {
-      console.error("Error uploading files:", error);
+      console.error("Error uploading file:", error);
     }
   };
 
@@ -67,9 +76,9 @@ export const Upload = () => {
     <div className="relative top-44 md:w-1/3 mx-auto w-full">
       <form
         onSubmit={handleSubmit}
-        className="mx-auto mt-16 bg-gradient-to-r from-red-600 to-red-800 text-white p-8 rounded-md shadow-md"
+        className="mx-auto mt-16 bg-gradient-to-r from-red-600 to-red-800 p-8 rounded-md shadow-md"
       >
-        <h1>Upload Documents</h1>
+        <h1 className="block mb-2">Upload Documents</h1>
         <label className="block mb-2">Title:</label>
         <input
           type="text"
@@ -77,6 +86,7 @@ export const Upload = () => {
           onChange={(e) => setTitle(e.target.value)}
           placeholder="Title"
           required
+          className="rounded-sm px-2 w-full"
         />
         <label className="block mb-2">Body:</label>
         <input
@@ -85,13 +95,32 @@ export const Upload = () => {
           onChange={(e) => setBody(e.target.value)}
           placeholder="Body"
           required
+          className="rounded-sm px-2 w-full"
         />
-        <label className="block mb-2">Documents:</label>
+        <label className="block mb-2">Category:</label>
+        <input
+          type="text"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          placeholder="Category"
+          required
+          className="rounded-sm px-2 w-full"
+        />
+        <label className="block mb-2">Sub Category:</label>
+        <input
+          type="text"
+          value={subCategory}
+          onChange={(e) => setSubCategory(e.target.value)}
+          placeholder="Sub Category"
+          required
+          className="rounded-sm px-2 w-full"
+        />
+        <label className="block mb-2">Document (PDF only):</label>
         <input
           type="file"
-          name="documents"
+          accept=".pdf"
           onChange={handleFileChange}
-          multiple
+          className="mb-2"
         />
         <button
           type="submit"
@@ -99,14 +128,6 @@ export const Upload = () => {
         >
           Upload
         </button>
-
-        <div>
-          {filePreview.map((preview, index) => (
-            <div key={index}>
-              <button onClick={() => handleFileRemove(index)}>Remove</button>
-            </div>
-          ))}
-        </div>
       </form>
     </div>
   );
