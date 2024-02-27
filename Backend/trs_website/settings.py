5cd12fb76cb3f8ca960506b358dffcea607cbcd0
Advait <126783821+Advaitgaur004@users.noreply.github.com>
2024-02-27 20:51:41 +0530

    Added PostgresSQL

--- Backend/trs_website/settings.py
+++ Backend/trs_website/settings.py
@@ -88,9 +88,13 @@ WSGI_APPLICATION = 'trs_website.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres', #Database name
+        'USER': 'postgres', #Login/group roles user(Have to create)
+        'PASSWORD': '1234', #Login/group roles password
+        'HOST': 'localhost',#default host
+        'PORT': '5432', #default port
     }
 }
 
